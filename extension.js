2dafef1f33f58ef1413f3ff7be1d2033c8da0e1d
UsmanHaroon1 <haroonasr9@gmail.com>
2022-08-31 14:31:21 +0530

    v1.0.0

--- extension.js
+++ extension.js
@@ -50,7 +50,7 @@ async function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Congratulations!Awesome job');
+		vscode.window.showInformationMessage('Congratulations V1.0.0!Awesome job');
 		// const article = await vscode.window.showQuickPick(articles, {
 		// 	matchOnDetail: true,
 		//   })
